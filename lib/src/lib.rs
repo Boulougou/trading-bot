ee011c3bedfbb266b980a615b761454b5b8f8d5d
boulougou <gboulougaris@gmail.com>
2020-10-12 01:24:11 +0300

    Added support for more timeframes when fetching history

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -43,6 +43,26 @@ pub enum HistoryTimeframe {
     Month1
 }
 
+impl HistoryTimeframe {
+    fn in_minutes(&self) -> u64 {
+        match self {
+            HistoryTimeframe::Min1 => 1,
+            HistoryTimeframe::Min5 => 5,
+            HistoryTimeframe::Min15 => 15,
+            HistoryTimeframe::Min30 => 30,
+            HistoryTimeframe::Hour1 => 60,
+            HistoryTimeframe::Hour2 => 2 * 60,
+            HistoryTimeframe::Hour3 => 3 * 60,
+            HistoryTimeframe::Hour4 => 4 * 60,
+            HistoryTimeframe::Hour6 => 6 * 60,
+            HistoryTimeframe::Hour8 => 8 * 60,
+            HistoryTimeframe::Day1 => 24 * 60,
+            HistoryTimeframe::Week1 => 7 * 24 * 60,
+            HistoryTimeframe::Month1 => 30 * 24 * 60
+        }
+    }
+}
+
 #[cfg_attr(test, automock)]
 pub trait TradingService {
     fn get_trade_symbols(&mut self) -> anyhow::Result<Vec<String>>;
@@ -71,28 +91,25 @@ pub fn fetch_symbol_history(service : &mut impl TradingService,
                             timeframe : HistoryTimeframe,
                             since_date : &DateTime<Utc>,
                             to_date : &DateTime<Utc>) -> anyhow::Result<()> {
-    if timeframe != HistoryTimeframe::Min1 {
-        todo!("Only Min1 timeframe is supported at the moment");
-    }
-
     let max_steps_per_call = service.max_history_steps_per_call()?;
+    let minutes_per_step = timeframe.in_minutes() as i64;
 
     let duration = to_date.signed_duration_since(*since_date);
-    let steps_needed = duration.num_minutes();
+    let steps_needed = duration.num_minutes() / minutes_per_step;
     let requests_needed = steps_needed / max_steps_per_call as i64;
 
     let mut history = Vec::new();
     for i in 0..requests_needed {
-        let request_from_date = since_date.add(Duration::minutes(i * max_steps_per_call as i64));
-        let request_to_date = request_from_date.add(Duration::minutes(max_steps_per_call as i64));
+        let request_from_date = since_date.add(Duration::minutes(i * minutes_per_step * max_steps_per_call as i64));
+        let request_to_date = request_from_date.add(Duration::minutes(minutes_per_step * max_steps_per_call as i64));
         let mut steps = service.get_symbol_history(symbol, timeframe, &request_from_date, &request_to_date)?;
         history.append(&mut steps);
     }
 
     let last_request_steps = steps_needed % max_steps_per_call as i64;
     if last_request_steps > 0 {
-        let request_from_date = since_date.add(Duration::minutes(requests_needed * max_steps_per_call as i64));
-        let request_to_date = request_from_date.add(Duration::minutes(last_request_steps));
+        let request_from_date = since_date.add(Duration::minutes(minutes_per_step * requests_needed * max_steps_per_call as i64));
+        let request_to_date = request_from_date.add(Duration::minutes(minutes_per_step * last_request_steps));
         let mut steps = service.get_symbol_history(symbol, timeframe, &request_from_date, &request_to_date)?;
         history.append(&mut steps);
     }
@@ -215,6 +232,68 @@ mod tests {
             .return_once(|_, _| Ok(()));
 
         fetch_symbol_history(&mut service, &mut storage, "EUR/CAN", HistoryTimeframe::Min1, &since_date, &to_date)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn fetch_history_using_min5_timeframe() -> anyhow::Result<()> {
+        let mut service = MockTradingService::new();
+        let mut storage = MockStorage::new();
+
+        let max_history_steps_per_call : u32 = 10;
+        service.expect_max_history_steps_per_call()
+            .return_once(move || Ok(max_history_steps_per_call));
+
+        let since_date = Utc.ymd(2019, 1, 1).and_hms(21, 30, 0);
+        let to_date = since_date.add(Duration::minutes(5 * max_history_steps_per_call as i64 + 5));
+        service.expect_get_symbol_history()
+            .with(eq("EUR/CAN"), eq(HistoryTimeframe::Min5), eq(since_date), eq(since_date.add(Duration::minutes(5 * max_history_steps_per_call as i64))))
+            .times(1)
+            .return_once(move |_, _, _, _| Ok(build_history(max_history_steps_per_call)));
+        service.expect_get_symbol_history()
+            .with(eq("EUR/CAN"), eq(HistoryTimeframe::Min5), eq(since_date.add(Duration::minutes(5 * max_history_steps_per_call as i64))),
+                eq(to_date))
+            .times(1)
+            .return_once(|_, _, _, _| Ok(build_history(1)));
+
+        storage.expect_save_symbol_history()
+            .with(eq("EUR_CAN_Min5_201901012130_201901012225"), eq(build_history(max_history_steps_per_call + 1)))
+            .times(1)
+            .return_once(|_, _| Ok(()));
+
+        fetch_symbol_history(&mut service, &mut storage, "EUR/CAN", HistoryTimeframe::Min5, &since_date, &to_date)?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn fetch_history_using_hour1_timeframe() -> anyhow::Result<()> {
+        let mut service = MockTradingService::new();
+        let mut storage = MockStorage::new();
+
+        let max_history_steps_per_call : u32 = 10;
+        service.expect_max_history_steps_per_call()
+            .return_once(move || Ok(max_history_steps_per_call));
+
+        let since_date = Utc.ymd(2019, 1, 1).and_hms(21, 30, 0);
+        let to_date = since_date.add(Duration::minutes(60 * max_history_steps_per_call as i64 + 60));
+        service.expect_get_symbol_history()
+            .with(eq("EUR/CAN"), eq(HistoryTimeframe::Hour1), eq(since_date), eq(since_date.add(Duration::minutes(60 * max_history_steps_per_call as i64))))
+            .times(1)
+            .return_once(move |_, _, _, _| Ok(build_history(max_history_steps_per_call)));
+        service.expect_get_symbol_history()
+            .with(eq("EUR/CAN"), eq(HistoryTimeframe::Hour1), eq(since_date.add(Duration::minutes(60 * max_history_steps_per_call as i64))),
+                eq(to_date))
+            .times(1)
+            .return_once(|_, _, _, _| Ok(build_history(1)));
+
+        storage.expect_save_symbol_history()
+            .with(eq("EUR_CAN_Hour1_201901012130_201901020830"), eq(build_history(max_history_steps_per_call + 1)))
+            .times(1)
+            .return_once(|_, _| Ok(()));
+
+        fetch_symbol_history(&mut service, &mut storage, "EUR/CAN", HistoryTimeframe::Hour1, &since_date, &to_date)?;
 
         Ok(())
     }
